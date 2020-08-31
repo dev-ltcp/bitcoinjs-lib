71b7fd6812d6da6fe916de4b585c60aeebb334ba
RomyToklio <contact@evaveo.com>
2020-08-31 11:41:28 +0200

    network fix

--- src/networks.js
+++ src/networks.js
@@ -1,35 +1,35 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.bitcoin = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bc',
+  messagePrefix: '\x18Litecoin PoS Signed Message:\n',
+  bech32: 'lp',
   bip32: {
-    public: 0x0488b21e,
-    private: 0x0488ade4,
+    public: 0x7788B21E,
+    private: 0x7788ADE4,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
+  pubKeyHash: 0x50,
+  scriptHash: 0x110,
   wif: 0x80,
 };
 exports.regtest = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcrt',
+  messagePrefix: '\x18Litecoin PoS Signed Message:\n',
+  bech32: 'lpt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
+  pubKeyHash: 0x65,
+  scriptHash: 0xc78,
   wif: 0xef,
 };
 exports.testnet = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'tb',
+  messagePrefix: '\x18Litecoin PoS Signed Message:\n',
+  bech32: 'lt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
+  pubKeyHash: 0x65,
+  scriptHash: 0x127,
   wif: 0xef,
 };
